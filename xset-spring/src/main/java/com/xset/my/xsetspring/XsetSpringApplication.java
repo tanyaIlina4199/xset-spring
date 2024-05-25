16e248197c6d193a9c55dee735ff013d17d46a04
tanyaIlina <4199ti@gmail.com>
2024-05-25 22:35:36 +0400

    init commit

---
+++ xset-spring/src/main/java/com/xset/my/xsetspring/XsetSpringApplication.java
@@ -0,0 +1,13 @@
+package com.xset.my.xsetspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsetSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsetSpringApplication.class, args);
+    }
+
+}
---
+++ xset-spring/src/test/java/com/xset/my/xsetspring/XsetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xset.my.xsetspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsetSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
